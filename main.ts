496ce5578e9382358d44440fcb7d958bd9ca351a
Felix Queisler <felix@queisler.de>
2025-04-04 11:52:02 +0200

    Mistral AI code as init

--- main.ts
+++ main.ts
@@ -1,134 +1,121 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RecursiveNoteDeleterSettings {
+  confirmDeletion: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RecursiveNoteDeleterSettings = {
+  confirmDeletion: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RecursiveNoteDeleter extends Plugin {
+  settings: RecursiveNoteDeleterSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+	await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	this.addRibbonIcon('trash', 'Delete Linked Notes', () => {
+	  const activeFile = this.app.workspace.getActiveFile();
+	  if (activeFile) {
+		this.deleteLinkedNotes(activeFile);
+	  } else {
+		new Notice('No active file found.');
+	  }
+	});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	this.addSettingTab(new RecursiveNoteDeleterSettingTab(this.app, this));
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  async deleteLinkedNotes(file: TFile) {
+	const linkedFiles = this.getLinkedFiles(file);
+	if (linkedFiles.length === 0) {
+	  new Notice('No linked notes or attachments found.');
+	  return;
 	}
 
-	onunload() {
-
+	if (this.settings.confirmDeletion) {
+	  const confirm = await this.confirmDeletion(linkedFiles);
+	  if (!confirm) {
+		return;
+	  }
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	linkedFiles.forEach(linkedFile => {
+	  this.app.vault.delete(linkedFile);
+	});
+	new Notice('Linked notes and attachments deleted.');
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+  getLinkedFiles(file: TFile): TFile[] {
+	const linkedFiles: TFile[] = [];
+	const cache = this.app.metadataCache.getFileCache(file);
+	if (cache) {
+	  const links = cache.links;
+	  links.forEach(link => {
+		const linkedFile = this.app.metadataCache.getFirstLinkpathDest(link.link, file.path);
+		if (linkedFile) {
+		  linkedFiles.push(linkedFile);
+		  linkedFiles.push(...this.getLinkedFiles(linkedFile)); // Recursive call
+		}
+	  });
 	}
+	return linkedFiles;
+  }
+
+  async confirmDeletion(files: TFile[]): Promise<boolean> {
+	const fileList = files.map(file => file.path).join('\n');
+	return new Promise((resolve) => {
+	  new Notice(`Are you sure you want to delete the following files?\n${fileList}`, 5000);
+	  const confirmButton = document.createElement('button');
+	  confirmButton.textContent = 'Confirm';
+	  confirmButton.onclick = () => {
+		resolve(true);
+	  };
+	  document.body.appendChild(confirmButton);
+
+	  const cancelButton = document.createElement('button');
+	  cancelButton.textContent = 'Cancel';
+	  cancelButton.onclick = () => {
+		resolve(false);
+	  };
+	  document.body.appendChild(cancelButton);
+	});
+  }
+
+  onunload() {
+	// Clean up code here
+  }
+
+  async loadSettings() {
+	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+	await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class RecursiveNoteDeleterSettingTab extends PluginSettingTab {
+  plugin: RecursiveNoteDeleter;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: RecursiveNoteDeleter) {
+	super(app, plugin);
+	this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+	const {containerEl} = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	new Setting(containerEl)
+	  .setName('Confirm Deletion')
+	  .setDesc('Ask for confirmation before deleting linked notes and attachments.')
+	  .addToggle(toggle => toggle
+		.setValue(this.plugin.settings.confirmDeletion)
+		.onChange(async (value) => {
+		  this.plugin.settings.confirmDeletion = value;
+		  await this.plugin.saveSettings();
+		}));
+  }
 }
